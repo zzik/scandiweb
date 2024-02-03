0a3c8122c42e04504da09b4a61b5489ced6245b7
zzik <edin.base@outlook.com>
2024-02-03 07:46:56 +0100

    add product handler component

---
+++ client/src/components/Buttons/Add.jsx
@@ -0,0 +1,10 @@
+
+import {routeHandler} from '../../utils/routeHandler'
+
+const Add = () => {
+  return (
+    <button onClick={()=>{routeHandler('addproduct')}}>ADHD</button>
+  )
+}
+
+export default Add
