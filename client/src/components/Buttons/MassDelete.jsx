2c61c94c8318ce99f2180484348d48dcb0a086ed
zzik <edin.base@outlook.com>
2024-02-03 07:47:34 +0100

    mass delete component

---
+++ client/src/components/Buttons/MassDelete.jsx
@@ -0,0 +1,8 @@
+
+import { deleteHandler } from "../../utils/deleteHandler"
+
+const MassDelete = () => {
+  return <button onClick={deleteHandler} id="delete-product-btn" >MASS DELETE</button>
+}
+
+export default MassDelete
